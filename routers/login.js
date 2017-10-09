84f80fed8a4781f5d45b610d4e791be1ec8edb09
wisnudj <jayalahwisnu@gmail.com>
2017-10-10 06:35:37 +0700

    login crypto

--- routers/login.js
+++ routers/login.js
@@ -1,13 +1,38 @@
 let express = require('express');
 let router = express.Router();
 let model = require('../models');
+const crypto = require('crypto');
+let alpanumerik =  require('../helper/alpanumerik')
 
 router.get('/', (req, res) => {
-  res.render('login')
+  res.render('login', {msgError: ''})
 })
 
 router.post('/', (req, res) => {
-  model.Users.findOne({
+  if(!req.body.username || !req.body.password) {
+    res.render('login', {msgError: 'Belum terisi'})
+  }
+  else {
+    model.Users.findOne({where: {username: req.body.username}}).then(dataUser => {
+       let secret = dataUser.salt
+       let nakedPassword = req.body.password;
+       const passwordInput = crypto.createHmac('sha256', secret)
+                            .update(nakedPassword)
+                            .digest('hex');
+      //res.send(dataUser)
+      if(passwordInput == dataUser.password) {
+        req.session.username = dataUser.username
+        req.session.role = dataUser.role
+        res.redirect('/index')
+      }
+      else {
+        res.render('login', {msgError: 'password salah'})
+      }
+    }).catch(() => {
+      res.render('login', {msgError: 'error'})
+    })
+  }
+  /*model.Users.findOne({
     where : {
       username: req.body.username
     }
@@ -21,7 +46,7 @@ router.post('/', (req, res) => {
     }
   }).catch(()=>{
       res.render('login')
-  })
+  }) */
   // model.Users.findAll().then((dataUsers) => {
   //   for(var i = 0; i < dataUsers.length; i++) {
   //     if(req.body.username == dataUsers[i].username && req.body.password == dataUsers[i].password) {
