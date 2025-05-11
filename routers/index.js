5ada8fefad539c31822355a79de81bb821916954
wisnudj <jayalahwisnu@gmail.com>
2025-05-11 19:15:27 +0700

    replace old project js to typescript

--- routers/index.js
+++
@@ -1,9 +0,0 @@
-let express = require('express');
-let router = express.Router();
-
-
-router.get('/',(req, res)=>{
-  res.render('index', {session:req.session, title: 'Home'});
-  //res.redirect('/login')
-})
-module.exports = router;
